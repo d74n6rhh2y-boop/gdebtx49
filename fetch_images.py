02b22b687aba25ffe23b48869fc8e6693092cb93
d74n6rhh2y-boop <omelettes.nickels_2i@icloud.com>
2026-06-25 18:54:57 +0300

    Update fetch_images.py

--- fetch_images.py
+++ fetch_images.py
@@ -27,7 +27,7 @@ GAMES = os.path.join(HERE, "games.json")
 IMG_DIR = os.path.join(HERE, "img")
 IMG_EXTS = (".jpg", ".jpeg", ".png", ".webp", ".gif", ".avif")
 
-UA = "Mozilla/5.0 (compatible; hexplay-bot/2.0; +https://hexplay.games)"
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
 TIMEOUT = 15
 
 USE_GOOGLE_PLAY = True
