35426157439de8d4baedebd65c484d3ab8f1c65d
d74n6rhh2y-boop <omelettes.nickels_2i@icloud.com>
2026-06-26 15:11:03 +0300

    Update navigation links with SVG icons

--- layout.js
+++ layout.js
@@ -14,8 +14,8 @@
         '<span class="u">hexplay</span><span style="color:var(--cyan)">_</span>' +
       '</a>' +
       '<nav class="nav">' +
-        '<a href="/" class="nav-btn' + on('games') + '">games</a>' +
-        '<a href="/about" class="nav-btn' + on('about') + '">about</a>' +
+        '<a href="/" class="nav-btn' + on('games') + '"><svg class="nav-ic" viewBox="0 0 24 24" aria-hidden="true"><line x1="6" x2="10" y1="11" y2="11"/><line x1="8" x2="8" y1="9" y2="13"/><line x1="15" x2="15.01" y1="12" y2="12"/><line x1="18" x2="18.01" y1="10" y2="10"/><path d="M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z"/></svg>games</a>' +
+        '<a href="/about" class="nav-btn' + on('about') + '"><svg class="nav-ic" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>about</a>' +
       '</nav>' +
     '</header>'
   );
